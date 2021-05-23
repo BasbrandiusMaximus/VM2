facedbfb60d447c1701b19dd3c6317e6b42b70d1
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2021-05-23 15:03:59 +0200

    Inventory verandert + lb.cfg

---
+++ playbooks/roles/web/files/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <br>
+        <br>
+        <p>
+            Hallo! Dit is mijn template pagina. Het werkt!
+        </p>
+        <br>
+        <? echo $_SERVER["REMOTE_ADDR"]; ?>
+    </body>
+</html>
