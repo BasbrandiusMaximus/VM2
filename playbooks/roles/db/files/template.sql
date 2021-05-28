fedeaae939633476936336b7ee1c1a6814da1237
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2021-05-28 20:31:45 +0200

    Added sql

--- playbooks/roles/db/files/template.sql
+++ playbooks/roles/db/files/template.sql
@@ -3,4 +3,13 @@ CREATE TABLE webdb (
     message varchar(255) NOT NULL
 )   ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO webdb (message) VALUES ('Woobaloobadoob');
+INSERT INTO webdb (message) VALUES ('Hey,');
+INSERT INTO webdb (message) VALUES ('ik');
+INSERT INTO webdb (message) VALUES ('denk');
+INSERT INTO webdb (message) VALUES ('dat');
+INSERT INTO webdb (message) VALUES ('het');
+INSERT INTO webdb (message) VALUES ('werkt.');
+INSERT INTO webdb (message) VALUES ('-');
+INSERT INTO webdb (message) VALUES ('Hiep');
+INSERT INTO webdb (message) VALUES ('Hiep');
+INSERT INTO webdb (message) VALUES ('Hoera!');
